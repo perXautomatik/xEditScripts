4317f714313b4857d4300a69e5024d85b676250d
perXautomatik <christoffer.broback@gmail.com>
2023-04-30 22:22:05 +0200

    state

--- Report masters.pas
+++ Report masters.pas
@@ -81,10 +81,44 @@ begin
   end;
 end;
 
-function Finalize: integer;
+{
+  Changes references of selected records to point to another provided FormID
+}
+
+
+var
+  NewFormID: Cardinal;
+
+function Initialize: integer;
+var
+  s: string;
 begin
-  slCheckMasters.Free;
-  sl.Free;
+  s := InputBox('Enter', 'New FormID for references', '');
+  if s = '' then begin
+    Result := 1;
+    Exit;
+  end;
+
+  NewFormID := StrToIntDef('$' + s, 0);
+
+  if NewFormID = 0 then begin
+    Result := 1;
+    Exit;
+  end;
 end;
+  
+function Process(e: IInterface): integer;
+var
+  OldFormID: Cardinal;
+  i: integer;
+begin
+  OldFormID := GetLoadOrderFormID(e);
+
+  // change formid of references
+  for i := Pred(ReferencedByCount(e)) downto 0 do
+    CompareExchangeFormID(ReferencedByIndex(e, i), OldFormID, NewFormID);
+end;
+
+
 
 end.
