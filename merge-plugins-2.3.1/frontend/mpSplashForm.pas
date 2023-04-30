6403b2fd76af83e61ee9e74bb5a82d12cd2f7945
perXautomatik <christoffer.broback@gmail.com>
2023-04-30 21:52:11 +0200

    Current

---
+++ merge-plugins-2.3.1/frontend/mpSplashForm.pas
@@ -0,0 +1,56 @@
+unit mpSplashForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ComCtrls, ExtCtrls, pngimage,
+  // mte units
+  mteTracker, mteHelpers,
+  // smash units
+  msConfiguration;
+
+type
+  TSplashForm = class(TForm)
+    lblTitle: TLabel;
+    imgSplash: TImage;
+    lblAuthor: TLabel;
+    lblProgress: TLabel;
+    lblVersion: TLabel;
+    procedure ProgressMessage(const s: string);
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  SplashForm: TSplashForm;
+
+implementation
+
+{$R *.dfm}
+
+procedure TSplashForm.ProgressMessage(const s: string);
+begin
+  lblProgress.Caption := '  '+s;
+  Application.ProcessMessages;
+end;
+
+procedure TSplashForm.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  Tracker.OnLogEvent := nil;
+end;
+
+procedure TSplashForm.FormCreate(Sender: TObject);
+begin
+  Tracker.OnLogEvent := ProgressMessage;
+  lblVersion.Caption := 'v'+GetVersionMem;
+
+  if settings.simpleSplash then
+    lblProgress.GlowSize := 0;
+end;
+
+end.
